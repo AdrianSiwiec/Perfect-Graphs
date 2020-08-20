c1fcdb9c26f8a56f8ac8ed84754f9653de0d6f31
Adrian Siwiec <adrian.siwiec@gmail.com>
2020-08-20 17:43:23 +0000

    Speed up cudaMemAllocHost and global dev mem

--- code/inc/commons.h
+++ code/inc/commons.h
@@ -30,7 +30,9 @@ struct vec : public vector<T> {
 
   // Range checking
   T &operator[](int i) { return vector<T>::at(i); }
+  // T &operator[](int i) { return vector<T>::operator[](i); }
   const T &operator[](int i) const { return vector<T>::at(i); }
+  // const T &operator[](int i) const { return vector<T>::operator[](i); }
 };
 template <typename T>
 ostream &operator<<(ostream &os, vec<T> const &v) {
@@ -120,9 +122,8 @@ vec<int> getCompleteVertices(const Graph &G, const vec<int> &X);
 
 // Runs dfs on a Graph G, with visited as an input-output of visited vertices. In addition action(v) will be
 // performed on each visited vertex. Each visited vertex (except start) must satisfy test predicate.
-void dfsWith(
-    const Graph &G, vec<int> &visited, int start, function<void(int)> action,
-    function<bool(int)> test = [](int v) -> bool { return true; });
+void dfsWith(const Graph &G, vec<int> &visited, int start, function<void(int)> action,
+             function<bool(int)> test = [](int v) -> bool { return true; });
 
 // Returns a vector of all components of G.
 vec<vec<int>> getComponents(const Graph &G);
