2bb500f4f81ac76c64ae9aa38df2105e433cdc55
Adrian Siwiec <adrian.siwiec@gmail.com>
2020-09-06 08:37:37 +0000

    New tests

--- code/test/cuPerfTest/run.sh
+++ code/test/cuPerfTest/run.sh
@@ -11,17 +11,26 @@ cuPerfectExec="$scriptDir/../../obj/test/cuPerfTest/cuPerfect.e"
 naiveExec="$scriptDir/../../obj/test/cuPerfTest/naive.e"
 
 perfectTests=(
-  # "perf2.t.in"
-  # "perf.t.in"
-  # "bench.t.in"
-  # "fullBinaryBig.t.in"
-  "fullBinary20to55.t.in"
-  "grid6by5to9.t.in"
-  # "knightGraph6by5to8.t.in"
-  "knightGraph8by4to8.t.in"
-  "hypercubes16to50.t.in"
-  # "kingGraph3by3to12.t.in"
-  "rookGraph5by4to7.t.in"
+  # # "perf2.t.in"
+  # # "perf.t.in"
+  # # "bench.t.in"
+  # # "fullBinaryBig.t.in"
+  # # "knightGraph6by5to8.t.in"
+  # "knightGraph8by4to8.t.in"
+  # "hypercubes16to50.t.in"
+  # # "kingGraph3by3to12.t.in"
+  # "rookGraph5by4to7.t.in"
+
+  # "fullBinary20to100.t.in"
+  # "grid6by5to11.t.in"
+)
+
+pefectSupplement=(
+  "grid6by5to11.t.in"
+)
+
+cudaPerfectSupplement=(
+  "grid6by5to11.t.in"
 )
 
 naiveTests=(
@@ -33,25 +42,39 @@ benchTests=(
   "hypercubes30to40.t.in"
 )
 
+
+# for file in "${perfectTests[@]}"; do
+#   echo "Running Perfect on $file"
+#   $perfectExec <$testsDir/$file >>$resultsDir/$file.out.csv
+# done
+
+# for file in "${perfectTests[@]}"; do
+#   echo "Running CuPerfect on $file"
+#   $cuPerfectExec <$testsDir/$file >>$resultsDir/$file.out.csv
+# done
+
+# for file in "${perfectTests[@]}"; do
+#   echo "Running Naive on $file"
+#   $naiveExec <$testsDir/$file >>$resultsDir/$file.out.csv
+# done
+
+# for file in "${pefectSupplement[@]}"; do
+#   echo "Running Perfect on $file"
+#   $perfectExec <$testsDir/perfectSup/$file >>$resultsDir/$file.out.csv
+# done
+
+for file in "${cudaPerfectSupplement[@]}"; do
+  echo "Running CuPerfect on $file"
+  $cuPerfectExec <$testsDir/cuPerfectSup/$file >>$resultsDir/$file.out.csv
+done
+
+
+
 # for file in "${benchTests[@]}"; do
 #   echo "Running Naive on $file"
 #   $naiveExec <$testsDir/$file >$resultsDir/$file.out.csv
 # done
 
-for file in "${perfectTests[@]}"; do
-  echo "Running Perfect on $file"
-  $perfectExec <$testsDir/$file >>$resultsDir/$file.out.csv
-done
-
-for file in "${perfectTests[@]}"; do
-  echo "Running CuPerfect on $file"
-  $cuPerfectExec <$testsDir/$file >>$resultsDir/$file.out.csv
-done
-
-for file in "${perfectTests[@]}"; do
-  echo "Running Naive on $file"
-  $naiveExec <$testsDir/$file >>$resultsDir/$file.out.csv
-done
 
 # for file in "${perfectTests[@]}"; do
 #   echo "Running CuPerfectNaive on $file"
--- code/test/generate/generate.cpp
+++ code/test/generate/generate.cpp
@@ -26,25 +26,27 @@ int main() {
   //   }
   // }
 
-  // int minN = 30;
-  // int maxN = 40;
-  // for (int k = minN; k <= maxN; k++) {
+  // int minN = 20;
+  // int maxN = 100;
+  // for (int k = minN; k <= maxN; k+=5) {
   //   Graph G = getFullBinaryTree(k);
   //   G.printOut();
   // }
 
+  // vec<vec<int>> sizes = {{6, 5}, {6, 6}, {6, 7}, {6, 8}, {6, 9}, {6, 10}, {6, 11}};
+  vec<vec<int>> sizes = {{6,14}, {6, 15}, {6, 16}, {6, 17}};
   // vec<vec<int>> sizes = {{5, 4}, {5, 5}, {5, 6}, {5, 7}};
-  // for (auto size : sizes) {
-  //   // Graph G = getCityGrid(size[0], size[1]);
-  //   // Graph G = getKnightGraph(size[0], size[1]);
-  //   Graph G = getRookGraph(size[0], size[1]);
-  //   G.printOut();
-  // }
-
-  for (int k = 30; k <= 40; k++) {
-    Graph G = getHypercube(k);
+  for (auto size : sizes) {
+    Graph G = getCityGrid(size[0], size[1]);
+    // Graph G = getKnightGraph(size[0], size[1]);
+    // Graph G = getRookGraph(size[0], size[1]);
     G.printOut();
   }
+
+  // for (int k = 30; k <= 40; k++) {
+  //   Graph G = getHypercube(k);
+  //   G.printOut();
+  // }
 
   // for (int k = 3; k <= 12; k++) {
   //   Graph G = getGridWithMoves(3, k, {1, -1, 0, 0, 1, 1, -1, -1}, {0, 0, 1, -1, 1, -1, 1, -1});
